b6f7de3382403acc06d180c800ce44ca6fafa496
Daniel Rivas Perez <drivas12@googlemail.com>
2020-03-06 21:57:13 +0000

    Improve lazy-load experience on slow internet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import { render } from "preact";
 import { useState, useRef, useEffect } from "preact/hooks";
 import { html } from "htm/preact";
 
+function useMedia(query) {
+  const [state, setState] = useState(matchMedia(query).matches);
+  useEffect(() => {
+    const handler = () => setState(matchMedia(query).matches);
+    window.addEventListener("resize", handler);
+    return () => {
+      window.removeEventListener("resize", handler);
+    };
+  }, [query]);
+  return state;
+}
+
 function useIntersectionObs({ root = null, rootMargin, threshold = 0 }) {
   const [info, updateInfo] = useState({});
   const [node, setNode] = useState(null);
@@ -32,7 +44,7 @@ function useIntersectionObs({ root = null, rootMargin, threshold = 0 }) {
   return [{ ref: setNode }, info];
 }
 
-function Img({ src, sm, md, lg, alt, aspectRatio, bg }) {
+function Img({ xs, sm, md, lg, alt, aspectRatio, bg }) {
   const [wasVisible, setWasVisible] = useState(false);
   const [hasLoaded, setHasLoaded] = useState(false);
   const [{ ref }, { isIntersecting }] = useIntersectionObs({});
@@ -41,29 +53,57 @@ function Img({ src, sm, md, lg, alt, aspectRatio, bg }) {
     if (isIntersecting) setWasVisible(true);
   }, [isIntersecting]);
 
+  const isLg = useMedia("(min-width: 1200px)");
+  const isMd = useMedia("(min-width: 800px)");
+  const isSm = useMedia("(min-width: 600px)");
+  const isXs = useMedia("(min-width: 400px)");
+
+  useEffect(() => {
+    let current = true;
+
+    if (!hasLoaded && wasVisible) {
+      const img = new Image();
+      img.onload = function() {
+        if (current) {
+          console.log("It loaded!");
+          setHasLoaded(true);
+        }
+      };
+      console.log("xs", isXs, "md", isMd, "sm", isSm, "lg", lg);
+      if (isLg) {
+        img.src = lg;
+      } else if (isMd) {
+        img.src = md;
+      } else if (isSm) {
+        img.src = sm;
+      } else {
+        img.src = xs;
+      }
+    }
+
+    return () => {
+      current = false;
+    };
+  });
+
   return html`
     <div
       ref=${ref}
       class="relative"
       style="padding-top: ${aspectRatio}%; background: url(data:image/gif;base64,${bg}); background-size: 100% 100%; background-repeat: no-repeat;"
     >
-      ${wasVisible &&
+      ${hasLoaded &&
         html`
           <picture>
             <source srcset=${lg} media="(min-width: 1200px)" />
             <source srcset=${md} media="(min-width: 800px)" />
-            <source srcset=${sm} />
+            <source srcset=${sm} media="(min-width: 600px)" />
+            <source srcset=${xs} media="(min-width: 400px)" />
             <img
-              onload=${() => {
-                console.log("Loaded!");
-                setHasLoaded(true);
-              }}
-              class="absolute w-full h-full left-0 top-0 ${hasLoaded
-                ? "fader"
-                : ""}"
-              style="width: 100%; height: 100% opacity: ${hasLoaded ? 1 : 0}"
+              class="absolute w-full h-full left-0 top-0 fader"
+              style="width: 100%; height: 100%"
               alt=${alt}
-              src=${src}
+              src=${lg}
             />
           </picture>
         `}
@@ -76,6 +116,7 @@ const lazyElements = document.getElementsByClassName("lazy");
 for (const elem of lazyElements) {
   const alt = elem.getAttribute("data-alt");
   const src = elem.getAttribute("data-src");
+  const xs = elem.getAttribute("data-xs");
   const sm = elem.getAttribute("data-sm");
   const md = elem.getAttribute("data-md");
   const lg = elem.getAttribute("data-lg");
@@ -86,6 +127,7 @@ for (const elem of lazyElements) {
     html`
       <${Img}
         alt=${alt}
+        xs=${xs}
         sm=${sm}
         md=${md}
         lg=${lg}
